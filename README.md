1156460ff248b58c8652391160d305d0e442934b
akash bhalke <akash.bhalke@spicemoney.com>
2023-01-08 15:37:33 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Basic Serverless Lambda using the Serverless Framework & GitHub Actions
+
+### YouTube Tutorial
+* https://www.youtube.com/watch?v=oFYFqOzJdqY
+#
---
+++ src/handler.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports.generateRandomNumber = event => {
+  
+  const randomNumber = parseInt(Math.random() * 100);
+  console.log("The random generated integer is: ", randomNumber);
+  return randomNumber;
+};
