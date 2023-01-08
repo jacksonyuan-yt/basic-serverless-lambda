01f01d8700ef32ff000649adce2b984b3d391040
akash bhalke <akashbhalked@users.noreply.github.com>
2023-01-08 15:35:05 +0530

    Delete handler.js

--- src/handler.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports.generateRandomNumber = event => {
-  
-  const randomNumber = parseInt(Math.random() * 100);
-  console.log("The random generated integer is: ", randomNumber);
-  return randomNumber;
-};
